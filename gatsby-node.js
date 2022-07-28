49f4709f3b8f7027c4ae95f9fb8fb0099bf1f0f5
Keketso Matsuma <49682202+MatsumaKeketso@users.noreply.github.com>
2022-07-28 17:26:15 +0200

    tag  & some responsiveness

--- gatsby-node.js
+++ gatsby-node.js
@@ -85,7 +85,7 @@ exports.createPages = async ({ actions, graphql }) => {
     if (node.frontmatter.tags.length) {
       node.frontmatter.tags.forEach((tag) => {
         createPage({
-          path: `/news/tag/${tag.replace(" ", "_")}`,
+          path: `/news/tag/${tag.replaceAll(" ", "_")}`,
           component: TagsTemplate,
           context: { tag: tag },
         });
