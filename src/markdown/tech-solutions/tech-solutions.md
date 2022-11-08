21207e6f656a1229f4d4036bb7b829c62d3f697f
Keketso Matsuma <49682202+MatsumaKeketso@users.noreply.github.com>
2022-11-08 17:30:49 +0200

    hotjar integration

--- src/markdown/tech-solutions/tech-solutions.md
+++ src/markdown/tech-solutions/tech-solutions.md
@@ -1,10 +1,4 @@
 ---
-<<<<<<< Updated upstream
-open: false
-link: https://redux-toolkit.js.org/tutorials/quick-start
----
-=======
 open: true
 link: ""
 ---
->>>>>>> Stashed changes
--- src/pages/pillers.js
+++ src/pages/pillers.js
@@ -76,6 +76,19 @@ const Pillers = ({ data, location }) => {
         setApplicationState({ state: open, link: link });
       }
     });
+    (function (h, o, t, j, a, r) {
+      h.hj =
+        h.hj ||
+        function () {
+          (h.hj.q = h.hj.q || []).push(arguments);
+        };
+      h._hjSettings = { hjid: 3238112, hjsv: 6 };
+      a = o.getElementsByTagName("head")[0];
+      r = o.createElement("script");
+      r.async = 1;
+      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+      a.appendChild(r);
+    })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
   }, []);
   return (
     <Layout>
