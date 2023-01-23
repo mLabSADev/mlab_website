4565c83deed18802a90cb88dc7c691812cfcbf95
Keketso Matsuma <keketso@mlab.co.za>
2023-01-23 10:26:00 +0200

    changes
    mLab Descriptor
    Resources

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,19 +25,19 @@ const IndexPage = ({ data }) => {
   const newsHeader = data.newsHeader.frontmatter.position;
   const newsAbstr = data.newsHeader.frontmatter.abstract;
   useEffect(() => {
-    (function (h, o, t, j, a, r) {
-      h.hj =
-        h.hj ||
-        function () {
-          (h.hj.q = h.hj.q || []).push(arguments);
-        };
-      h._hjSettings = { hjid: 3238112, hjsv: 6 };
-      a = o.getElementsByTagName("head")[0];
-      r = o.createElement("script");
-      r.async = 1;
-      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-      a.appendChild(r);
-    })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
+    // (function (h, o, t, j, a, r) {
+    //   h.hj =
+    //     h.hj ||
+    //     function () {
+    //       (h.hj.q = h.hj.q || []).push(arguments);
+    //     };
+    //   h._hjSettings = { hjid: 3238112, hjsv: 6 };
+    //   a = o.getElementsByTagName("head")[0];
+    //   r = o.createElement("script");
+    //   r.async = 1;
+    //   r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+    //   a.appendChild(r);
+    // })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
   }, []);
   return (
     <Layout>
